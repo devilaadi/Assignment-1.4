9dfc35090c17a2c03678b19cf0171f95ca6df6aa
AdityaB <AdityaB@SDGL810.sdgc.com>
2017-06-22 03:06:11 +0530

    First Commit

---
+++ src/Assignment1_4.java
@@ -0,0 +1,20 @@
+
+public class Assignment1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//ASCII codes from 65 to 90
+//Logic
+		
+	System.out.println("-------------ASCII codes from 65 to 90--------------");	
+		for(int i=65;i<91;i++)
+		{
+			char z =(char)i;
+			System.out.println(i+"-"+z);
+			
+		}
+		
+	}
+
+}
